61a74dd891ec34b0370755c0639b6bce9deba051
Kirkland <kirkland@yuknis.com>
2020-12-24 15:01:41 -0500

    Initial commit

---
+++ src/test/java/com/yuknis/extrip/ExtripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuknis.extrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
